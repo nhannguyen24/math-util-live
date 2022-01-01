fa0fd8d563053f5884cbd32370899958dee7c082
nhannguyen24 <dnhan2426@gmail.com>
2022-01-02 00:10:12 +0700

    Lan thu 2 lam chuyen ay - quen roi

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -12,6 +12,10 @@ public class MathUtilLive {
         
         System.out.println("expected: 0! = 1;   actual: " + MathUtil.getFactorial(0));
         
+        // Thêm code sau lần đầu tiên làm chuyện ấy - CODE lên Server 12:08 1/2/2022
+        System.out.println("expected: 1! = 1;   actual: " + MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6;   actual: " + MathUtil.getFactorial(3));
+        
         // kĩ thuật kiểm thử phần mềm: ước lượng xem giátrị hàm trả về của hàm là gì
         // expected value: 5! = 120
         // mày/ hàm ai đó viết khi chạy thực tế là mấy - actual, giả sử 120
